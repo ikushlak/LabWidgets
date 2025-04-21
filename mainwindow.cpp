9d22ef904122518b6a8d03be63525788dad57f57
ikushlak <ivkushlak@gmail.com>
2025-04-21 13:24:36 +0700

    кнопка "следующее" не работает

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,23 +6,19 @@
 #include <locale.h>
     mainwindow::mainwindow(QWidget *parent):QWidget(parent)
 {
-    codec = QTextCodec::codecForName("Windows-1251");
+    codec = QTextCodec::codecForName("UTF-8");
     setWindowTitle(codec->toUnicode("Возведение в квадрат"));
     frame = new QFrame(this);
     frame->setFrameShadow(QFrame::Raised);
     frame->setFrameShape(QFrame::Panel);
-    inputLabel = new QLabel(codec->toUnicode("Введите число:"),
-                            this);
+    inputLabel = new QLabel(codec->toUnicode("Введите число:"), this);
     inputEdit = new QLineEdit("",this);
     StrValidator *v=new StrValidator(inputEdit);
     inputEdit->setValidator(v);
-    outputLabel = new QLabel(codec->toUnicode("Результат:"),
-                             this);
+    outputLabel = new QLabel(codec->toUnicode("Результат:"), this);
     outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton(codec->toUnicode("Следующее"),
-                                 this);
-    exitButton = new QPushButton(codec->toUnicode("Выход"),
-                                 this);
+    nextButton = new QPushButton(codec->toUnicode("Следующее"), this);
+    exitButton = new QPushButton(codec->toUnicode("Выход"), this);
     // компоновка приложения выполняется согласно рисунку 2
     QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
     vLayout1->addWidget(inputLabel);
